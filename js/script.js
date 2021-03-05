898b15d68ac486a870cc03d7fd37368319582ead
Alessandro Marega <ale_marega@hotmail.it>
2021-03-05 17:46:26 +0100

    inserita variabile sum dentro all'evento

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var burgerNameInput = document.getElementById('burger-name-input');
 var buttonCalculate = document.getElementById('button-calculate');
 var total = document.getElementById('total');
 var ingredients = document.getElementsByClassName("ingredient");
-var sum = 50;
+
 var couponsList = ["123456", "H7F567", "897945", "458741"];
 var couponInput = document.getElementById('coupon-input');
 
@@ -11,13 +11,17 @@ var couponInput = document.getElementById('coupon-input');
 // aggiungo l'evento al click sul bottone 'sum'
 buttonCalculate.addEventListener( "click",
   function () {
+    var sum = 50;
     // controllo se burgerNameInput è non vuoto o meno
     if (burgerNameInput.value != "") {
       // inizializzo tutti gli elementi con la classe ingredient e li sommo alla cifra iniziale
       for (var i = 0; i < ingredients.length; i++) {
+
         if (ingredients[i].checked) {
+
           sum += parseInt(ingredients[i].value);
         }
+        console.log(sum);
       }
       // applico lo sconto dei coupon
       if (couponsList.includes(couponInput.value)) {
@@ -25,7 +29,6 @@ buttonCalculate.addEventListener( "click",
         // elimino il coupon utilizzato dalla lista di coupons
         couponsList.splice(couponsList.indexOf(couponInput.value), 1);
       }
-
       // stampo il totale
       total.innerHTML = sum + "	\u20AC";
     } else {
